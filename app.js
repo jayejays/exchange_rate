f7c27f6b97fb21251a3c627a64e052558a6722cf
Jason <jasonmadrigal96@gmail.com>
2017-11-29 04:20:11 +1300

    Updated user and pass.. Uploaded to azure

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: "a5263a19-dae3-48a9-ac35-20d4f7966698"
+    appPassword: "guXTY5540ehsfrBDIB7|)~+"
 });
 
 // Listen for messages from users 
